d12089d94ebffc7651349767780ecedaa4229f13
580nm <580nm@ZAEE>
2018-12-28 16:45:34 +0900

    vo

---
+++ WPlan/src/main/java/kr/co/mlec/cmember/vo/CmemberVO.java
@@ -0,0 +1,97 @@
+package kr.co.mlec.cmember.vo;
+
+public class CmemberVO {
+	private String id;
+	private String name;
+	private String password;
+	private String tel;
+	private String email;
+	private String cName;
+	private int cNO;
+	private String cAddress;
+	private String type;
+	private String regDate;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getcName() {
+		return cName;
+	}
+
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+
+	public int getcNO() {
+		return cNO;
+	}
+
+	public void setcNO(int cNO) {
+		this.cNO = cNO;
+	}
+
+	public String getcAddress() {
+		return cAddress;
+	}
+
+	public void setcAddress(String cAddress) {
+		this.cAddress = cAddress;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	
+	
+
+}
---
+++ WPlan/src/main/java/kr/co/mlec/qna/vo/QnaVO.java
@@ -0,0 +1,62 @@
+package kr.co.mlec.qna.vo;
+
+public class QnaVO {
+	
+	private int no;
+	private String id;
+	private String content;
+	private String regDate;
+	private int pNo;
+	private int qNo;
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getpNo() {
+		return pNo;
+	}
+
+	public void setpNo(int pNo) {
+		this.pNo = pNo;
+	}
+
+	public int getqNo() {
+		return qNo;
+	}
+
+	public void setqNo(int qNo) {
+		this.qNo = qNo;
+	}
+	
+	
+
+}
