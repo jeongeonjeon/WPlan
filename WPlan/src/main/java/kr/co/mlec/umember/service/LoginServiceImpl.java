9cc66ef8005e915fb75254b9477e0888c2034b2f
eon <eon@DESKTOP-AKNQC6Q>
2019-01-01 15:43:41 +0900

    error

--- WPlan/src/main/java/kr/co/mlec/umember/service/LoginServiceImpl.java
+++ WPlan/src/main/java/kr/co/mlec/umember/service/LoginServiceImpl.java
@@ -1,11 +1,11 @@
-package kr.co.mlec.login.service;
+package kr.co.mlec.umember.service;
 
 import org.apache.ibatis.javassist.bytecode.annotation.MemberValueVisitor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.co.mlec.login.dao.LoginDAO;
-import kr.co.mlec.member.vo.MemberVO;
+import kr.co.mlec.umember.dao.LoginDAO;
+import kr.co.mlec.umember.vo.UmemberVO;
 
 @Service
 public class LoginServiceImpl implements LoginService {
@@ -13,13 +13,13 @@ public class LoginServiceImpl implements LoginService {
 	@Autowired
 	private LoginDAO loginDao;
 	
-	@Override
-	public MemberVO login(MemberVO member) {
+	public UmemberVO login(UmemberVO member) {
 
-		MemberValueVisitor userVO = loginDao.login(member);
+		UmemberVO userVO = loginDao.login(member);
 		
 		return userVO;
 	}
+
 
 }
 
